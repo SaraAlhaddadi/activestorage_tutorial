e9647bc89620d8b69cbeb99a103f133db7859396
Sara Alhaddadi <sara.khalid.alhaddadi@gmail.com>
2023-09-23 19:42:58 +0300

    use variant and image processing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_one_attached :profile_photo
+  has_one_attached :profile_photo do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  # has_one_base64_attached :avatar do |attachable|
+  #   attachable.variant(:thumb, resize_to_limit: [100, 100])
+  # end
 end
---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,7 @@
+# config/initializers/active_storage.rb
+# Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
+# Rails.application.config.active_storage.analyzers = [ActiveStorage::Analyzer::ImageAnalyzer, ActiveStorage::Analyzer::VideoAnalyzer, ActiveStorage::Analyzer::AudioAnalyzer]
+Rails.application.config.active_storage.track_variants = true
+# Rails.application.config.active_storage.variant_processor = :mini_magick
+# ActiveStorage::Current.url_options = { host: 'localhost', port: 3000 }
+
